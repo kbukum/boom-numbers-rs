a2713fd0e32066a6296ff700fdc6454820428df8
kbukum <kamilbukum@gmail.com>
2017-03-31 17:15:55 +0300

    fixed comments.

--- src/main.rs
+++ src/main.rs
@@ -26,21 +26,19 @@ fn main() {
                             println!("Value is not between {}-{}", 0, 10i32.pow(7));
                             continue;
                     } else {
-                    
-                        /*
-                        let result:String = format!("{:b}", n - 1).chars()
-                        .map(|x| match x { 
-                            '0' => '2', 
-                            '1' => '3',
-                            _ => x}
-                        ).collect();
-                        */
+                        // shift 1 and get integer as bits
                         let formatted_result = format!("{:b}", n + 1);
+                        // regex to replace from 0 to something
                         let s1_re = Regex::new(r"0").unwrap();
+                        // regex to replace from 1 to something
                         let s2_re = Regex::new(r"1").unwrap();
+                        // get length of bits
                         let len = formatted_result.len();
+                        // remove first character.
                         let slice: String = formatted_result.chars().skip(1).take(len - 1).collect();
+                        // replace all 0 character as 2
                         let replaced_result = s1_re.replace_all(&slice, "2");
+                        // replace all 1 character as 3
                         println!("Result: {}", s2_re.replace_all(&replaced_result, "3")); // => "xxxxx xxxxx!"
 
                     }
